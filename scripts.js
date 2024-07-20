d576453fd54ea48a5f78c36a7c029280da9db872
vanhungsi <vanhungsi@gmail.com>
2024-07-20 15:12:25 +0700

    Create function playRound

--- scripts.js
+++ scripts.js
@@ -12,9 +12,44 @@ function getComputerChoice() {
 
 
 function getHumanChoice(){
-    let humanChoice = prompt('Please enter “rock”, “paper” or “scissors”',"");
+    let humanChoice = prompt('Please enter “rock”, “paper” or “scissors”',"").toLowerCase();
     return humanChoice;
 }
 
-getHumanChoice();
-console.log(getHumanChoice());
+// getHumanChoice();
+// console.log(getHumanChoice());
+
+let humanScore = (Number(""), 0);
+let computerScore = (Number(""), 0);
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        alert("Please enter the right word - 'rock', 'paper' or 'scissors'");
+
+    } else if (humanChoice === computerChoice) {
+        alert("Oh! It's a tie.");
+
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        alert("You lose this round, paper beats rock!");
+
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        alert("You win this round, rock beats scissors!");
+
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        alert("You win this round, paper beats rock!");
+
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        alert("You lose this round, scissors beats paper!");
+
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        alert("You lose this round, rock beats scissors!");
+
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        alert("You win this round, scissors beats paper!");
+    }
+
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
